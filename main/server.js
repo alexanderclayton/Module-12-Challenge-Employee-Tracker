ae77824f5e4c3c635cd384db41f4157736ab0ccb
Alex Clayton <alexander.clayton.email@gmail.com>
2022-12-05 17:41:44 -0600

    initial commit

---
+++ main/server.js
@@ -0,0 +1,13 @@
+const inquire = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: ''
+    },
+    console.log('Connected to database')
+)
